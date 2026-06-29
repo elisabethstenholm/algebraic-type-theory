88225a42be754193474f4846658df230049f6af6
Elisabeth Stenholm <elisabeth@stenholm.one>
2026-06-29 23:00:07 +0200

    Add nix flake

---
+++ flake.nix
@@ -0,0 +1,28 @@
+{
+  description = "Algebraic type theory";
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs";
+    flake-utils.url = "github:numtide/flake-utils";
+    unilib.url = "git+ssh://git@git.app.uib.no/Hakon.Gylterud/unilib.git?ref=main";
+    unilib.inputs.nixpkgs.follows = "nixpkgs";
+  };
+
+  outputs = { self, nixpkgs, flake-utils, unilib }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+        theLib = pkgs.agdaPackages.mkDerivation {
+          meta = {};
+          pname = "Algebraic type theory";
+          version = "1.0.0";
+          src = ./.;
+          buildInputs = [
+            unilib.packages.${system}.default
+          ];
+        };
+      in
+        {
+          packages.default = theLib;
+        });
+}
+
